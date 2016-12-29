8e80d9c1610caeb894a755d9acc783840fab3758
Mr_Jia <Mr_Jia@Mr-JiadeMacBook-Pro.local>
2016-12-29 10:26:08 +0800

    Initial Commit

---
+++ TabbarSwiper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabbarSwiper
+//
+//  Created by Mr_Jia on 2016/12/29.
+//  Copyright © 2016年 Mr_Jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TabbarSwiper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabbarSwiper
+//
+//  Created by Mr_Jia on 2016/12/29.
+//  Copyright © 2016年 Mr_Jia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
